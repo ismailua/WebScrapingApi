e311849c62a796ffeffec8b08397d50b13db892a
ismailua <ismailua@gmail.com>
2025-06-25 09:10:38 +0300

    Http Değişikliği

--- src/Client.php
+++ src/Client.php
@@ -1,7 +1,7 @@
 <?php
 namespace WebScrapingApi;
 
-use Symfony\Component\HttpClient\HttpClient;
+use Illuminate\Support\Facades\Http;
 
 class Client
 {
@@ -19,7 +19,7 @@ class Client
         $final_params = array("api_key" => $this->api_key, "url" => $url) + $params;
         $query = http_build_query($final_params);
         $final_url = $this->api_url."?".$query;
-        $httpClient = HttpClient::create();
+        $httpClient = Http::create();
 
         try {
             $http_response = $httpClient->request($method, $final_url,
